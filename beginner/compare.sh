4a432edc152a06239bcca90909c2969ab4798b5c
Tabbakha, Ahmad <Ahmad.tabbakha@toyotaconnected.com>
2018-08-29 13:48:00 -0600

    fix indentation

--- beginner/compare.sh
+++ beginner/compare.sh
@@ -3,9 +3,9 @@ S1='string'
 S2='String'
 if [ $S1=$S2 ];
 then
-        echo "S1('$S1') is not equal to S2('$S2')"
+    echo "S1('$S1') is not equal to S2('$S2')"
 fi
 if [ $S1=$S1 ];
 then
-        echo "S1('$S1') is equal to S1('$S1')"
+    echo "S1('$S1') is equal to S1('$S1')"
 fi
